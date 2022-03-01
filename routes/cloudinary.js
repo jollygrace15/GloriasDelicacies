372c7508c55acd286d354170090f8507d34684bc
jollygrace15 <jollygrace.c.4@gmail.com>
2022-03-01 14:54:13 +0000

    added a js script for create.hbs so cloudinary upload will work.

--- routes/cloudinary.js
+++ routes/cloudinary.js
@@ -13,7 +13,7 @@ router.get('/sign', async (req,res)=>{
     const params_to_sign = JSON.parse(req.query.params_to_sign);
 
     // retrieve our cloudinary api secret from the environment
-    const apiSecret = process.env.CLOUDINARY_API_SECRET
+    const apiSecret = process.env.CLOUDINARY_API_SECRET //iba
 
     // get the signature (aka CSRF)
     const signature = cloudinary.utils.api_sign_request(params_to_sign, apiSecret);
