5972adf313e41d27ba974b992c39d1b13b84f359
jollygrace15 <jollygrace.c.4@gmail.com>
2022-03-01 16:14:23 +0000

    figuring out why cloudinary widget does not work.

--- routes/cloudinary.js
+++ routes/cloudinary.js
@@ -13,7 +13,7 @@ router.get('/sign', async (req,res)=>{
     const params_to_sign = JSON.parse(req.query.params_to_sign);
 
     // retrieve our cloudinary api secret from the environment
-    const apiSecret = process.env.CLOUDINARY_API_SECRET //iba
+    const apiSecret = process.env.CLOUDINARY_API_SECRET
 
     // get the signature (aka CSRF)
     const signature = cloudinary.utils.api_sign_request(params_to_sign, apiSecret);
@@ -21,5 +21,4 @@ router.get('/sign', async (req,res)=>{
     res.send(signature);
 })
 
-
-module.exports = router; // #3 export out the router
+module.exports = router;
